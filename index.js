e1ba4330dcb11e295193e18e603d823288071960
Jean Osco <willjean29@gmail.com>
2023-11-08 15:43:32 -0500

    feat: add relations and list data

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ async function main() {
   // TODO: create users
   // const newUser = await prisma.user.create({
   //   data: {
-  //     name: 'Marcos',
-  //     email: 'marcos@pgmail.com',
+  //     name: 'Marcelo',
+  //     email: 'marcelo@pgmail.com',
   //   }
   // });
   // console.log({ newUser });
@@ -52,26 +52,62 @@ async function main() {
   // }
 
   // TODO: update or insert users
+  // try {
+  //   const user = await prisma.user.upsert({
+  //     where: {
+  //       email: 'jhon@gmail.com'
+  //     },
+  //     create: {
+  //       name: 'Jhon',
+  //       email: 'jhon@gmail.com'
+  //     },
+  //     update: {
+  //       lastName: 'Smith',
+  //     }
+  //   });
+  //   console.log({ user })
+  // } catch (error) {
+  //   console.log(error.message);
+  // }
 
-  try {
-    const user = await prisma.user.upsert({
-      where: {
-        email: 'jhon@gmail.com'
-      },
-      create: {
-        name: 'Jhon',
-        email: 'jhon@gmail.com'
-      },
-      update: {
-        lastName: 'Smith',
-      }
-    });
-    console.log({ user })
-  } catch (error) {
-    console.log(error.message);
-  }
+  // TODO: create new post 
+  // const post = await prisma.post.create({
+  //   data: {
+  //     title: 'My first post',
+  //     content: 'This is my first post',
+  //     author: {
+  //       connect: {
+  //         id: newUser.id,
+  //       }
+  //     }
+  //   }
+  // })
+  // console.log({ post })
 
+  // TODO: create new user with her posts
+  // const newUser = await prisma.user.create({
+  //   data: {
+  //     name: 'Ricardo',
+  //     email: 'ricardo@gmail.com',
+  //     posts: {
+  //       create: {
+  //         title: 'Snk',
+  //         content: 'Shingeky no kyojin',
+  //       }
+  //     }
+  //   }
+  // })
+  // console.log({ newUser });
+  // const posts = await prisma.post.findMany({});
+  // console.log({ posts });
 
+  // TODO: find user with her posts
+  const users = await prisma.user.findMany({
+    include: {
+      posts: true
+    }
+  });
+  console.log({ users })
 }
 
 main();
