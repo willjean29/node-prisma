6ae2a36967175a68342437ee4f9fd82d44596211
Jean Osco <willjean29@gmail.com>
2023-11-09 18:01:51 -0500

    feat(rest-api): add crud operations to products and categories

--- rest-api/src/routes/categories.routes.js
+++ rest-api/src/routes/categories.routes.js
@@ -1,5 +1,23 @@
 import { Router } from 'express';
+import { prisma } from '../db.js';
 
-export const categotiesRouter = Router();
+const router = Router();
 
+router.get("/categories", async (req, res) => {
+  const categories = await prisma.category.findMany();
+  res.json(categories)
+})
 
+router.post("/categories", async (req, res) => {
+  try {
+    const category = await prisma.category.create({
+      data: req.body
+    })
+    res.json(category)
+  } catch (error) {
+    console.log({ error });
+    res.status(500).json({ error })
+  }
+})
+
+export const categotiesRouter = router;
--- rest-api/src/routes/products.routes.js
+++ rest-api/src/routes/products.routes.js
@@ -1,11 +1,25 @@
 import { Router } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '../db.js';
 const router = Router();
-const prisma = new PrismaClient();
+
 
 router.get("/products", async (req, res) => {
   const products = await prisma.product.findMany();
   res.json(products)
+})
+
+router.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await prisma.product.findUnique({
+    where: {
+      id: Number(id)
+    },
+    include: {
+      category: true
+    }
+  });
+  if (!product) return res.status(404).json({ error: "Product not found" })
+  res.json(product)
 })
 
 router.post("/products", async (req, res) => {
@@ -18,7 +32,29 @@ router.post("/products", async (req, res) => {
     console.log({ error });
     res.status(500).json({ error })
   }
+})
 
+router.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await prisma.product.delete({
+    where: {
+      id: Number(id)
+    }
+  });
+  if (!product) return res.status(404).json({ error: "Product not found" })
+  res.json(product);
+})
+
+router.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await prisma.product.update({
+    where: {
+      id: Number(id)
+    },
+    data: req.body
+  });
+  if (!product) return res.status(404).json({ error: "Product not found" })
+  res.json(product);
 })
 
 export const productsRouter = router;
